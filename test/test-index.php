5174a2c55ea32b231ce64fbf435f7258ef867502
jabenninghoff <jbenninghoff@mac.com>
2001-12-28 03:37:03 -0600

    late-night commit

--- test/test-index.php
+++ test/test-index.php
@@ -1,19 +1,29 @@
 <?php
-// $Id: test-xml.php,v 1.3 2001/12/28 09:37:03 death Exp $
+// $Id: test-index.php,v 1.1 2001/12/28 09:37:03 death Exp $
 
+// create page, header & footer blocks 
 $doc = domxml_new_xmldoc("1.0");
 $page = $doc->add_root("page");
-$header = $page->new_child("header", "_header");
+$page->new_child("header", "_header");
+$page->new_child("footer", "_footer");
+
+// header & footer replacement goes here
+
+// sidebars go here
+
+// create & load main block from database
 $main_block = $page->new_child("block", "");
 $main_block->setattr("class","main");
 $main_block->setattr("id","1");
 $story = $main_block->new_child("story", "");
 
+
 $story->new_child("content", "_content_12");
-$tmpstr = "<p>this is where the <i>story</i> content goes.</p>";
+$tmpstr = "<p>this is where the <em>story</em> content goes.</p>";
 $tmpdoc = str_replace("_content_12", $tmpstr, $doc->dumpmem());
 $doc = xmldoc($tmpdoc);
 
+// display the web page
 print $doc->dumpformat();
 
 ?>
