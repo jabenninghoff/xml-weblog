2953f6aa2f6698383d97cd74608ce161a0c09646
jabenninghoff <jbenninghoff@mac.com>
2002-06-02 18:09:36 -0500

    xhtml template

---
+++ test/php_template.php
@@ -0,0 +1,13 @@
+<?php
+print '<?xml version="1.0" encoding="iso-8859-1"?>'."\n";
+?>
+<!DOCTYPE html  PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
+<!-- $Id: php_template.php,v 1.1 2002/06/02 23:09:36 loki Exp $ -->
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>Title</title>
+  </head>
+  <body>
+    <p>Empty Page</p>
+  </body>
+</html>
