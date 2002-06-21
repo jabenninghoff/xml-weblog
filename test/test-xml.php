4786db22c6c1db077664603b392a9af9475620d0
jabenninghoff <jbenninghoff@mac.com>
2002-06-21 14:54:06 -0500

    updated for php 4.2.1

--- test/test-xml.php
+++ test/test-xml.php
@@ -2,7 +2,7 @@
 print '<?xml version="1.0" encoding="iso-8859-1"?>'."\n";
 ?>
 <!DOCTYPE html  PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
-<!-- $Id: test-xml.php,v 1.4 2002/06/21 19:24:27 loki Exp $ -->
+<!-- $Id: test-xml.php,v 1.5 2002/06/21 19:54:06 loki Exp $ -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <title>Title</title>
@@ -11,18 +11,27 @@ print '<?xml version="1.0" encoding="iso-8859-1"?>'."\n";
     <pre>
 <?php
 $doc = domxml_new_doc("1.0");
-$page = $doc->create_element("page");
-$page = $doc->append_child($page);
-$header = $page->new_child("header", "_header");
-$main_block = $page->new_child("block", "");
-#$main_block->setattr("class","main");
-#$main_block->setattr("id","1");
-$story = $main_block->new_child("story", "");
 
-$story->new_child("content", "_content_12");
+$page = $doc->append_child($doc->create_element("page"));
+
+$header = $page->append_child($doc->create_element("header"));
+$header_text = $header->append_child($doc->create_text_node("_header"));
+
+$main_block = $page->append_child($doc->create_element("block"));
+$main_block->set_attribute("class","main");
+$main_block->set_attribute("id","1");
+
+$story = $main_block->append_child($doc->create_element("story"));
+
+$content = $story->append_child($doc->create_element("content"));
+$content_text = $content->append_child($doc->create_text_node("_content_12"));
+
+$footer = $page->append_child($doc->create_element("footer"));
+$footer_text = $footer->append_child($doc->create_text_node("_footer"));
+
 $tmpstr = "<p>this is where the <i>story</i> content goes.</p>";
-$tmpdoc = str_replace("_content_12", $tmpstr, $doc->dumpmem());
-$doc = xmldoc($tmpdoc);
+$tmpdoc = str_replace("_content_12", $tmpstr, $doc->dump_mem());
+$doc = domxml_open_mem($tmpdoc);
 
 print htmlentities($doc->dump_mem(true));
 ?>
