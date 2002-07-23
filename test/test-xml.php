880687cfeb537f901ee7c3dd68c4954bc20cd07c
jabenninghoff <jbenninghoff@mac.com>
2002-07-23 02:13:50 -0500

    block "main" -> main story -> article
    remove obsolete test-index

--- test/test-xml.php
+++ test/test-xml.php
@@ -3,7 +3,7 @@ print '<?xml version="1.0" encoding="iso-8859-1"?>'."\n";
 include "include/functions.inc.php";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
-<!-- $Id: test-xml.php,v 1.6 2002/07/20 16:15:23 loki Exp $ -->
+<!-- $Id: test-xml.php,v 1.7 2002/07/23 07:13:50 loki Exp $ -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <title>Title</title>
@@ -18,19 +18,17 @@ $page = $doc->append_child($doc->create_element("page"));
 $header = $page->append_child($doc->create_element("header"));
 $header_text = $header->append_child($doc->create_text_node("_header"));
 
-$main_block = $page->append_child($doc->create_element("block"));
-$main_block->set_attribute("class","main");
-$main_block->set_attribute("id","1");
+$main = $page->append_child($doc->create_element("main"));
 
-$story = $main_block->append_child($doc->create_element("story"));
+$article = $main->append_child($doc->create_element("article"));
 
-$content = $story->append_child($doc->create_element("content"));
+$content = $article->append_child($doc->create_element("content"));
 $content_text = $content->append_child($doc->create_text_node("_content_12"));
 
 $footer = $page->append_child($doc->create_element("footer"));
 $footer_text = $footer->append_child($doc->create_text_node("_footer"));
 
-$tmpstr = "<p>this is where the <i>story</i> content goes.</p>";
+$tmpstr = "<p>this is where the <i>article</i> content goes.</p>";
 $tmpdoc = str_replace("_content_12", $tmpstr, $doc->dump_mem());
 $doc = domxml_open_mem($tmpdoc);
 
