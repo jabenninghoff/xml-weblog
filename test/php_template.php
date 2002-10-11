a33867306b048a3263c83b39852cb196a9d10f5e
jabenninghoff <jbenninghoff@mac.com>
2002-10-11 15:14:19 -0500

    include --> include_once (better)

--- test/php_template.php
+++ test/php_template.php
@@ -1,9 +1,9 @@
 <?php
 print '<?xml version="1.0" encoding="iso-8859-1"?>'."\n";
-include "include/functions.inc.php";
+include_once "include/functions.inc.php";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
-<!-- $Id: php_template.php,v 1.3 2002/07/20 16:15:23 loki Exp $ -->
+<!-- $Id: php_template.php,v 1.4 2002/10/11 20:14:19 loki Exp $ -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <title>Title</title>
