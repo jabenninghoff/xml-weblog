1749f19ef3af164b1ca5c67a0b7fa722272ed531
jabenninghoff <jbenninghoff@mac.com>
2002-10-11 21:29:42 -0500

    use dynamic xml source

--- test/xslt_test.php
+++ test/xslt_test.php
@@ -3,18 +3,27 @@ print '<?xml version="1.0" encoding="iso-8859-1"?>'."\n";
 include_once "../include/functions.inc.php";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "/DTD/xhtml1-strict.dtd">
-<!-- $Id: xslt_test.php,v 1.2 2002/10/12 01:08:21 loki Exp $ -->
+<!-- $Id: xslt_test.php,v 1.3 2002/10/12 02:29:42 loki Exp $ -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <title>XSLT transform test</title>
   </head>
   <body>
 <?php
+// get php-formatted xml document
+$xmlData = fopen ("http://loki:luzer@www.technomagik.net/private/xml/template.php", "r");
+while ($line = fgets ($xmlData))
+    $xml .= $line;
+
+$arguments = array(
+     '/_xml' => $xml
+);
+
 // Allocate a new XSLT processor
 $xh = xslt_create();
 
 // Process the document, returning the result into the $result variable
-$result = xslt_process($xh, '../template.xml', '../style/basic_xhtml.xsl');
+$result = xslt_process($xh, 'arg:/_xml', '../style/basic_xhtml.xsl', NULL, $arguments);
 if ($result) {
     print "<p>SUCCESS, template.xml was transformed by basic_xhtml.xsl into the \$result";
     print " variable. The \$result variable has the following contents:</p>\n";
