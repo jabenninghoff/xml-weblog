faaa891f3c4c0f74b588fbb73382ffe653c351a2
jabenninghoff <jbenninghoff@mac.com>
2002-10-17 00:02:38 -0500

    fixed include search path

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
-// $Id: index.php,v 1.2 2002/10/16 19:33:00 loki Exp $
+// $Id: index.php,v 1.3 2002/10/17 05:02:38 loki Exp $
 // front page renderer
 
-// includes
-require_once "include/functions.inc.php";
+require_once "functions.inc.php";
 
 // get php-formatted xml document
 ob_start();
-require "xml/index.xml.php";
+require "index.xml.php";
 $xml = ob_get_contents();
 ob_end_clean();
 
