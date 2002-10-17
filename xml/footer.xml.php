fc384b53cb2ff118079c9342d7460f35becbc9a9
jabenninghoff <jbenninghoff@mac.com>
2002-10-17 00:04:15 -0500

    split up index into component parts and start to pull code from database

---
+++ xml/footer.xml.php
@@ -0,0 +1,23 @@
+<?php
+// $Id: footer.xml.php,v 1.1 2002/10/17 05:04:15 loki Exp $
+
+require_once "functions.inc.php";
+
+if (basename($_SERVER['PHP_SELF']) == "footer.xml.php") {
+    // standalone
+    header('Content-Type: text/xml');
+    echo '<?xml version="1.0" encoding="iso-8859-1" standalone="yes"?>',"\n";
+}
+?>
+  <!-- footer: bottom of page, includes disclaimer -->
+  <footer>
+    <disclaimer>
+      All trademarks and copyrights on this page are owned by their respective
+      owners. Comments are owned by the Poster. The Rest (c) 2002 tm.net
+      (site.disclaimer)
+    </disclaimer>
+    <content>
+      <?php validate_self(); ?>
+      <p>(site.footer_content,XHTML+,optional)</p>
+    </content>
+  </footer>
---
+++ xml/sidebar.xml.php
@@ -0,0 +1,34 @@
+<?php
+// $Id: sidebar.xml.php,v 1.1 2002/10/17 05:04:15 loki Exp $
+
+require_once "functions.inc.php";
+
+if (basename($_SERVER['PHP_SELF']) == "sidebar.xml.php") {
+    // standalone
+    header('Content-Type: text/xml');
+    echo '<?xml version="1.0" encoding="iso-8859-1" standalone="yes"?>',"\n";
+}
+?>
+  <!-- left or right sidebar(s), outermost is index 0 -->
+  <sidebar align="left" index="0">
+
+    <!-- zero or more blocks, topmost is index 0 -->
+    <block index="0">
+      <title>News Sites (block.title)</title>
+      <content>
+        <a href="http://www.openbsd.org/">OpenBSD Journal</a><br/>
+        <a href="http://daily.daemonnews.org/">daemonnews</a><br/>
+        <a href="http://slashdot.org/">Slashdot</a><br/>
+        (block.content,XHTML+)
+      </content>
+    </block>
+
+    <block index="1">
+      <title>Bogus Block (block.title)</title>
+      <content>
+        This block is <b>bogus!!!</b>
+        (block.content,XHTML+)
+      </content>
+    </block>
+
+  </sidebar>
