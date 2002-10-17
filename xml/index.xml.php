47cbb40917b9a2044cab70c6c872492321c02ac8
jabenninghoff <jbenninghoff@mac.com>
2002-10-17 00:04:15 -0500

    split up index into component parts and start to pull code from database

--- xml/index.xml.php
+++ xml/index.xml.php
@@ -1,59 +1,23 @@
 <?php
-// require_once only if not called from somewhere else
+// $Id: index.xml.php,v 1.3 2002/10/17 05:04:15 loki Exp $
+require_once "functions.inc.php";
+require_once "config.inc.php";
+
 if (basename($_SERVER['PHP_SELF']) == "index.xml.php") {
-    require_once "../include/functions.inc.php";
+    // standalone
+    header('Content-Type: text/xml');
 }
+
+// get site info 
+$site = $db->getRow("select * from site where id=1", DB_FETCHMODE_ASSOC);
 ?>
 <?xml version="1.0" encoding="iso-8859-1" standalone="yes"?>
-<!-- $Id: index.xml.php,v 1.2 2002/10/16 22:13:25 loki Exp $ -->
 <!-- XML-weblog front page -->
-
-<!-- page: XML-weblog root element -->
 <page lang="en" title="technomagik.net (generated)">
 
-  <!-- header: top of the page, with logo, slogan, etc.  -->
-  <header>
-    <banner>ad-banner-here (image,generated,optional)</banner>
-    <name>technomagik.net (site.name)</name>
-    <slogan>We need a slogan! (XHTML+,site.slogan)</slogan>
-    <logo>(site.logo,image,optional)</logo>
-    <url>http://www.technomagik.net</url><!-- site.url -->
-    <description>random weblog. (site.description)</description>
-    <content>
-      Any content, like a <a href="http://www.google.com/">search page</a>,
-      could go here.
-      (site.header_content,XHTML+,optional)
-    </content>
-    <!-- zero or more messages, topmost is index 0 -->
-    <message index="0">
-      <b>still under development... not open yet!</b>
-      (site.message,XHTML,optional)
-    </message>
-  </header>
+<?php require "header.xml.php"; ?>
 
-  <!-- left or right sidebar(s), outermost is index 0 -->
-  <sidebar align="left" index="0">
-
-    <!-- zero or more blocks, topmost is index 0 -->
-    <block index="0">
-      <title>News Sites (block.title)</title>
-      <content>
-        <a href="http://www.openbsd.org/">OpenBSD Journal</a><br/>
-        <a href="http://daily.daemonnews.org/">daemonnews</a><br/>
-        <a href="http://slashdot.org/">Slashdot</a><br/>
-        (block.content,XHTML+)
-      </content>
-    </block>
-
-    <block index="1">
-      <title>Bogus Block (block.title)</title>
-      <content>
-        This block is <b>bogus!!!</b>
-        (block.content,XHTML+)
-      </content>
-    </block>
-
-  </sidebar>
+<?php require "sidebar.xml.php"; ?>
 
   <!-- main: main section of document. index page contains articles. -->
   <main>
@@ -88,16 +52,6 @@ if (basename($_SERVER['PHP_SELF']) == "index.xml.php") {
     </article>
   </main>
 
-  <!-- footer: bottom of page, includes disclaimer -->
-  <footer>
-    <disclaimer>
-      All trademarks and copyrights on this page are owned by their respective
-      owners. Comments are owned by the Poster. The Rest (c) 2002 tm.net
-      (site.disclaimer)
-    </disclaimer>
-    <content>
-      <?php validate_self(); ?>
-      <p>(site.footer_content,XHTML+,optional)</p>
-    </content>
-  </footer>
+<?php require "footer.xml.php"; ?>
+
 </page>
