1a1d95548fe95bb90f38ab342eeac3626a03b1cf
jabenninghoff <jbenninghoff@mac.com>
2002-10-17 00:45:33 -0500

    more better include path

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-// $Id: index.php,v 1.3 2002/10/17 05:02:38 loki Exp $
+// $Id: index.php,v 1.4 2002/10/17 05:45:33 loki Exp $
 // front page renderer
 
-require_once "functions.inc.php";
+require_once "include/functions.inc.php";
 
 // get php-formatted xml document
 ob_start();
-require "index.xml.php";
+require "xml/index.xml.php";
 $xml = ob_get_contents();
 ob_end_clean();
 
