d602d9d039f73230a6afb53b0b36fd2f67a8f6f3
jabenninghoff <jbenninghoff@mac.com>
2002-10-17 02:13:49 -0500

    clean up header, footer, implement dynamic code using xml-like <?code include="file.php"?> processing instruction

--- xml/footer.xml.php
+++ xml/footer.xml.php
@@ -1,23 +1,26 @@
 <?php
-// $Id: footer.xml.php,v 1.2 2002/10/17 05:45:33 loki Exp $
+// $Id: footer.xml.php,v 1.3 2002/10/17 07:13:49 loki Exp $
 
 require_once "include/functions.inc.php";
+require_once "include/config.inc.php";
 
 if (basename($_SERVER['PHP_SELF']) == "footer.xml.php") {
     // standalone
     header('Content-Type: text/xml');
     echo '<?xml version="1.0" encoding="iso-8859-1" standalone="yes"?>',"\n";
+    // get site info 
+    $site = $db->getRow("select * from site where id=1", DB_FETCHMODE_ASSOC);
 }
 ?>
   <!-- footer: bottom of page, includes disclaimer -->
   <footer>
     <disclaimer>
-      All trademarks and copyrights on this page are owned by their respective
-      owners. Comments are owned by the Poster. The Rest (c) 2002 tm.net
-      (site.disclaimer)
+<?php echo $site['disclaimer'], "\n"; ?>
     </disclaimer>
     <content>
-      <?php validate_self(); ?>
-      <p>(site.footer_content,XHTML+,optional)</p>
+<?php
+echo "<!-- ", $site['footer_content'], "-->\n";
+echo process_code($site['footer_content']);
+?>
     </content>
   </footer>
--- xml/header.xml.php
+++ xml/header.xml.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: header.xml.php,v 1.2 2002/10/17 05:45:33 loki Exp $
+// $Id: header.xml.php,v 1.3 2002/10/17 07:13:49 loki Exp $
 
 require_once "include/functions.inc.php";
 require_once "include/config.inc.php";
@@ -15,15 +15,15 @@ if (basename($_SERVER['PHP_SELF']) == "header.xml.php") {
   <!-- header: top of the page, with logo, slogan, etc.  -->
   <header>
     <banner>[banner: not implemented]</banner>
+    <logo>[logo: not implemented]</logo>
 <?php
-$element = array ( "name", "slogan", "logo", "url", "description" );
+$element = array ( "name", "slogan", "url", "description" );
 foreach ($element as $tag) {
     echo "<$tag>", $site[$tag], "</$tag>\n";
 }
 ?>
-    <content>
-<?php echo $site['header_content'] ?>
-    </content>
+    <content><?php echo $site['header_content'] ?></content>
+
     <!-- zero or more messages, topmost is index 0 -->
     <message index="0">
       <b>still under development... not open yet!</b>
