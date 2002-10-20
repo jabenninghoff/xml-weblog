6550804d87e2467c55007c8eba909efa16b54d6d
jabenninghoff <jbenninghoff@mac.com>
2002-10-19 19:34:54 -0500

    include cleanup

--- image.php
+++ image.php
@@ -1,8 +1,7 @@
 <?php
-// $Id: image.php,v 1.3 2002/10/19 21:04:52 loki Exp $
+// $Id: image.php,v 1.4 2002/10/20 00:34:54 loki Exp $
 // image renderer
 
-require_once "include/functions.inc.php";
 require_once "include/config.inc.php";
 
 $q = "select * from image where name='".basename($_GET['name'])."'";
--- xml/article.xml.php
+++ xml/article.xml.php
@@ -1,7 +1,8 @@
 <?php
-// $Id: article.xml.php,v 1.3 2002/10/19 21:04:52 loki Exp $
-require_once "include/functions.inc.php";
+// $Id: article.xml.php,v 1.4 2002/10/20 00:34:54 loki Exp $
+
 require_once "include/config.inc.php";
+require_once "include/functions.inc.php";
 
 if (basename($_SERVER['PHP_SELF']) == "article.xml.php") {
     // standalone
