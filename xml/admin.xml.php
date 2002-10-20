781c8269a713a386e67801d6bcd7dbf6524fe4a3
jabenninghoff <jbenninghoff@mac.com>
2002-10-19 19:34:54 -0500

    include cleanup

--- xml/admin.xml.php
+++ xml/admin.xml.php
@@ -1,7 +1,8 @@
 <?php
-// $Id: admin.xml.php,v 1.2 2002/10/19 18:36:55 loki Exp $
-require_once "include/functions.inc.php";
+// $Id: admin.xml.php,v 1.3 2002/10/20 00:34:54 loki Exp $
+
 require_once "include/config.inc.php";
+require_once "include/functions.inc.php";
 
 if (basename($_SERVER['PHP_SELF']) == "admin.xml.php") {
     // standalone
