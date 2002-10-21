0af30445d12894feae42e08f8bf8058a57ed0b69
jabenninghoff <jbenninghoff@mac.com>
2002-10-21 01:18:36 -0500

    validate image name

--- image.php
+++ image.php
@@ -1,10 +1,12 @@
 <?php
-// $Id: image.php,v 1.4 2002/10/20 00:34:54 loki Exp $
+// $Id: image.php,v 1.5 2002/10/21 06:18:36 loki Exp $
 // image renderer
 
 require_once "include/config.inc.php";
+require_once "include/functions.inc.php";
 
-$q = "select * from image where name='".basename($_GET['name'])."'";
+$name = safe_gpc_addslashes($_GET['name']);
+$q = "select * from image where name='".$name."'";
 $image = $db->getRow($q, DB_FETCHMODE_ASSOC);
 
 header("Content-Type: image/gif");
