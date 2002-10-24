0de2f2ecf3eadaa2459438bba826b80c02d4801d
jabenninghoff <jbenninghoff@mac.com>
2002-10-24 17:38:35 -0500

    Cancel button works ! Stupid IE doesn't handle <BUTTON> properly; it is always successful !! We must use <INPUT type="submit"> instead. This works in IE & Mozilla.

--- xml/admin.xml.php
+++ xml/admin.xml.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: admin.xml.php,v 1.8 2002/10/24 20:27:51 loki Exp $
+// $Id: admin.xml.php,v 1.9 2002/10/24 22:38:35 loki Exp $
 
 require_once "include/config.inc.php";
 require_once "include/functions.inc.php";
@@ -20,13 +20,13 @@ $display = array(
     "XHTML-long" => false
 );
 
-function admin_input($name, $type, $value) {
-    global $req_mode;
+function admin_input($name, $type, $value, $mode) {
 
     echo "          <td><b>", ucfirst($name), "</b></td>\n";
 
-    if ($req_mode == "delete") {
-        echo "<td>$value</td>\n";
+    if ($mode == "delete") {
+        echo "<td>$value", '<input name="', $name, '" type="hidden" value="',
+            $value, '"/>', "</td>\n";
         return;
     }
 
@@ -76,6 +76,77 @@ function admin_input($name, $type, $value) {
     echo "</td>\n";
 }
 
+function display_form()
+{
+global $db,$type,$display;
+
+// display variables
+$get_mode = $_GET['mode'];
+if ($get_mode != "edit" && $get_mode != "delete") $get_mode = "create";
+
+$get_type = $_GET['type'];
+if (!in_array($get_type,$type)) $get_type = $type[0];
+
+$get_id = valid_ID($_GET['id']);
+
+$schema = $db->getAll("select distinct property,datatype from schema where ".
+    "object='$get_type'", DB_FETCHMODE_ASSOC);
+
+$object = fetch_type($get_type);
+
+$get_object = $db->getRow("select * from $get_type where id='$get_id'",
+    DB_FETCHMODE_ASSOC);
+
+if ($get_mode == "create") {
+    echo "      <title>", ucfirst($get_type."s"), "</title>\n";
+    // only display object list in create mode
+    foreach ($object as $obj) {
+        echo "      <object type=\"$get_type\">\n";
+        foreach ($schema as $s) {
+            if ($display[$s['datatype']]) {
+                echo "        ";
+                echo "<property name=\"{$s['property']}\">",
+                    "{$obj[$s['property']]}</property>\n";
+            }
+        }
+    echo "        <property><a href=\"?type=$get_type&amp;mode=edit&amp;id=",
+        $obj['id'], "\">edit</a></property>\n";
+    echo "        <property><a href=\"?type=$get_type&amp;mode=delete&amp;id=",
+        $obj['id'], "\">delete</a></property>\n";
+        echo "      </object>\n";
+    }
+} else {
+    echo "      <title>", ucfirst($get_mode), " ", ucfirst($get_type),
+        "</title>\n";
+}
+echo "      <form action=\"{$_SERVER['PHP_SELF']}?type=$get_type\" ",
+    "method=\"post\">\n";
+echo "      <table>\n";
+foreach ($schema as $s) {
+    echo "        <tr>\n";
+    admin_input($s['property'], $s['datatype'],
+        htmlspecialchars($get_object[$s['property']]), $get_mode);
+    echo "        </tr>\n";
+}
+echo "      </table>\n";
+echo '      <input name="mode" type="hidden" value="', $get_mode, '"/>',"\n";
+echo '      <input name="type" type="hidden" value="', $get_type, '"/>',"\n";
+if ($get_mode == "edit") $button = "Save";
+else $button = ucfirst($get_mode);
+echo '      <p><input name="submit" type="submit" value="', $button, '"/> ',
+    '<input name="cancel" type="submit" value="Cancel"/></p>', "\n";
+echo "      </form>\n";
+}
+
+function process_form()
+{
+echo "<form><table>\n";
+foreach ($_POST as $key => $value) {
+    echo "<tr><td><b>$key</b></td><td>$value</td></tr>\n";
+}
+echo "</table></form>\n";
+}
+
 if (basename($_SERVER['PHP_SELF']) == "admin.xml.php") {
     // standalone
     header('Content-Type: text/xml');
@@ -94,22 +165,6 @@ $block = fetch_block();
 // admin.php variables
 $type = $db->getCol("select distinct object from schema group by object");
 
-$req_mode = $_GET['mode'];
-if ($req_mode != "edit" && $req_mode != "delete") $req_mode = "create";
-
-$req_type = $_GET['type'];
-if (!in_array($req_type,$type)) $req_type = $type[0];
-
-$req_id = valid_ID($_GET['id']);
-
-$schema = $db->getAll("select distinct property,datatype from schema where ".
-    "object='$req_type'", DB_FETCHMODE_ASSOC);
-
-$object = fetch_type($req_type);
-
-$req_object = $db->getRow("select * from $req_type where id='$req_id'",
-    DB_FETCHMODE_ASSOC);
-
 ?>
 <?xml version="1.0" encoding="iso-8859-1" standalone="yes"?>
 <?php echo "<!-- {$req_object['id']} -->\n"; ?>
@@ -126,47 +181,11 @@ $req_object = $db->getRow("select * from $req_type where id='$req_id'",
 foreach ($type as $item) {
     echo "      <menu link=\"?type=$item\">",ucfirst($item."s"),"</menu>\n";
 }
-
-if ($req_mode == "create") {
-    echo "      <title>", ucfirst($req_type."s"), "</title>\n";
-    // only display object list in create mode
-    foreach ($object as $obj) {
-        echo "      <object type=\"$req_type\">\n";
-        foreach ($schema as $s) {
-            if ($display[$s['datatype']]) {
-                echo "        ";
-                echo "<property name=\"{$s['property']}\">",
-                    "{$obj[$s['property']]}</property>\n";
-            }
-        }
-    echo "        <property><a href=\"?type=$req_type&amp;mode=edit&amp;id=",
-        $obj['id'], "\">edit</a></property>\n";
-    echo "        <property><a href=\"?type=$req_type&amp;mode=delete&amp;id=",
-        $obj['id'], "\">delete</a></property>\n";
-        echo "      </object>\n";
-    }
+if (isset($_POST['submit'])) {
+    process_form();
 } else {
-    echo "      <title>", ucfirst($req_mode), " ", ucfirst($req_type),
-        "</title>\n";
+    display_form();
 }
-echo "      <form action=\"{$_SERVER['PHP_SELF']}\" method=\"post\">\n";
-echo "      <table>\n";
-foreach ($schema as $s) {
-    echo "        <tr>\n";
-    admin_input($s['property'], $s['datatype'],
-        htmlspecialchars($req_object[$s['property']]));
-    echo "        </tr>\n";
-}
-echo "      </table>\n";
-echo '      <input name="mode" type="hidden" value="', $req_mode, '"/>',"\n";
-echo '      <input name="type" type="hidden" value="', $req_type, '"/>',"\n";
-if ($req_mode == "edit") $button = "Save";
-else $button = ucfirst($req_mode);
-echo '      <p><button name="button" type="submit" value="submit">',
-    $button, '</button> <button name="button" type="submit" value="cancel">',
-    "Cancel</button></p>\n";
-echo "      </form>\n";
-
 ?>
     </admin>
   </main>
