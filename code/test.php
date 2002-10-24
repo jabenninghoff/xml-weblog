f126f6c905eb4f61e8873a480f2c4f551162dcd7
jabenninghoff <jbenninghoff@mac.com>
2002-10-24 18:00:03 -0500

    more better validity check

--- code/test.php
+++ code/test.php
@@ -1,3 +1,8 @@
 <?php
-validate_self();
+//validate_self();
 ?>
+  <p>
+    <a href="http://validator.w3.org/check/referer"><img
+        src="image.php?name=valid-xhtml10.png"
+        alt="Valid XHTML 1.0!" height="31" width="88" /></a>
+  </p>
