d778dcb9e16bb2213987bdb3b76c9359a1498035
jabenninghoff <jbenninghoff@mac.com>
2002-10-27 01:46:16 -0600

    milestone 2 reached ! there is a really annoying bug in the image type code -- slashes disappear ??

--- image.php
+++ image.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: image.php,v 1.5 2002/10/21 06:18:36 loki Exp $
+// $Id: image.php,v 1.6 2002/10/27 07:46:16 loki Exp $
 // image renderer
 
 require_once "include/config.inc.php";
@@ -9,6 +9,6 @@ $name = safe_gpc_addslashes($_GET['name']);
 $q = "select * from image where name='".$name."'";
 $image = $db->getRow($q, DB_FETCHMODE_ASSOC);
 
-header("Content-Type: image/gif");
+header("Content-Type: ".$image['type']);
 echo $image['src'];
 ?>
