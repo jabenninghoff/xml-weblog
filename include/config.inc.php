eec0a4c139a93bb0e9471e4de98b21fa95cdc0c7
jabenninghoff <jbenninghoff@mac.com>
2002-10-28 11:23:13 -0600

    massive style/code cleanup. move all database/SQL to db.inc.php for functional abstraction implemented proper site-checking add missing Id tags use xlw_ prefix for systemwide globals config.inc.php now contains only configuration variables move xml declaration to xml_declaration function improved validate_self() works with GET query string

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,5 +1,14 @@
 <?php
-// connect
-require_once('DB.php');
-$db = DB::connect("mysql://xml:weblog@localhost/xml_tmnet", true);
+// $Id: config.inc.php,v 1.2 2002/10/28 17:23:13 loki Exp $
+
+// database configuration
+$xlw_db_type = "mysql";         // currently, only mysql is supported
+$xlw_db_server = "localhost";
+$xlw_db_user = "xml";
+$xlw_db_password = "weblog";
+$xlw_db_database = "xml_tmnet";
+
+// defaults
+$xlw_article_default_limit = 10;
+
 ?>
