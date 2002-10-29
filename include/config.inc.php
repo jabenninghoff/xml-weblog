fa93c0830ec74bc28164920e85a632e78e6b617f
jabenninghoff <jbenninghoff@mac.com>
2002-10-29 12:19:46 -0600

    new sample site

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,12 +1,12 @@
 <?php
-// $Id: config.inc.php,v 1.2 2002/10/28 17:23:13 loki Exp $
+// $Id: config.inc.php,v 1.3 2002/10/29 18:19:46 loki Exp $
 
 // database configuration
 $xlw_db_type = "mysql";         // currently, only mysql is supported
 $xlw_db_server = "localhost";
 $xlw_db_user = "xml";
 $xlw_db_password = "weblog";
-$xlw_db_database = "xml_tmnet";
+$xlw_db_database = "xml_weblog";
 
 // defaults
 $xlw_article_default_limit = 10;
