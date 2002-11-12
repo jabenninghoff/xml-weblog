98ea045c0ef6b7941613b015102dd34c968bc74f
jabenninghoff <jbenninghoff@mac.com>
2002-11-12 16:31:00 -0600

    Move sample site images & icons out of the database to make the page load faster. Keep the empty tables to allow remote loading of icons/images via web administration.

--- install/sample-values.sql
+++ install/sample-values.sql
@@ -1,4 +1,4 @@
--- $Id: sample-values.sql,v 1.14 2002/11/05 08:34:20 loki Exp $
+-- $Id: sample-values.sql,v 1.15 2002/11/12 22:31:05 loki Exp $
 --
 -- XML-weblog sample values
 --
@@ -44,32 +44,32 @@ INSERT INTO block VALUES (2,'left',1,2,'Administration','<a href=\"admin.php\">l
 
 INSERT INTO message VALUES (1,1,'0000-00-00 00:00:00','0000-00-00 00:00:00','<b>Under Construction:</b> This site has not yet been configured.','en');
 
-INSERT INTO site VALUES (1,'http://www.xml-weblog.org/',10,'xml-weblog','the first fully buzzword-compliant weblog engine','image.php?name=xml-weblog-logo.png','xml-weblog sample site','<p><code include=\"date.php\"/></p>',' All trademarks and copyrights on this page are owned by their respective owners.<br/>Comments are owned by the Poster. The Rest &#169; 2002 xml-weblog.org','<p>\r\n<a class=\"img\" href=\"http://httpd.apache.org\"><img src=\"image.php?name=apache_pb.png\" alt=\"Powered by Apache\"/></a>\r\n<a class=\"img\" href=\"http://www.mysql.com\"><img src=\"image.php?name=mysql_pb.png\" alt=\"Powered by MySQL\"/></a>\r\n<a class=\"img\" href=\"http://www.php.net\"><img src=\"image.php?name=php_pb.png\" alt=\"Powered by PHP\"/></a>\r\n<code include=\"validxml.php\"/>\r\n<a class=\"img\" href=\"http://jigsaw.w3.org/css-validator/\"><img style=\"border:0;width:88px;height:31px\" src=\"image.php?name=valid-css.png\" alt=\"Valid CSS!\"/></a>\r\n</p>','en');
+INSERT INTO site VALUES (1,'http://www.xml-weblog.org/',10,'xml-weblog','the first fully buzzword-compliant weblog engine','images/xml-weblog-logo.png','xml-weblog sample site','<p><code include=\"date.php\"/></p>',' All trademarks and copyrights on this page are owned by their respective owners.<br/>Comments are owned by the Poster. The Rest &#169; 2002 xml-weblog.org','<p>\r\n<a class=\"img\" href=\"http://httpd.apache.org\"><img src=\"images/apache_pb.png\" alt=\"Powered by Apache\"/></a>\r\n<a class=\"img\" href=\"http://www.mysql.com\"><img src=\"images/mysql_pb.png\" alt=\"Powered by MySQL\"/></a>\r\n<a class=\"img\" href=\"http://www.php.net\"><img src=\"images/php_pb.png\" alt=\"Powered by PHP\"/></a>\r\n<code include=\"validxml.php\"/>\r\n<a class=\"img\" href=\"http://jigsaw.w3.org/css-validator/\"><img style=\"border:0;width:88px;height:31px\" src=\"images/valid-css.png\" alt=\"Valid CSS!\"/></a>\r\n</p>','en');
 
-INSERT INTO topic VALUES (1,'No Topic','No topic.','icon.php?name=blank.png');
-INSERT INTO topic VALUES (2,'Announcements','Site Information and Announcements.','icon.php?name=announce.png');
-INSERT INTO topic VALUES (3,'Apple','Apple Computer, Mac OS X, and all things Macintosh.','icon.php?name=apple.png');
-INSERT INTO topic VALUES (4,'BSD','BSD and Berkeley-derived Unices: FreeBSD, NetBSD, OpenBSD, and Darwin.','icon.php?name=bsd.png');
-INSERT INTO topic VALUES (5,'Books','Book news and reviews.','icon.php?name=books.png');
-INSERT INTO topic VALUES (6,'Bugs','xml-weblog bug reports.','icon.php?name=bugs.png');
-INSERT INTO topic VALUES (7,'Censorship','censorship and attacks on free speech principles.','icon.php?name=censorship.png');
-INSERT INTO topic VALUES (8,'Courts','The Courts and Legal matters.','icon.php?name=courts.png');
-INSERT INTO topic VALUES (9,'Games','Computer, board, card, and other games.','icon.php?name=games.png');
-INSERT INTO topic VALUES (10,'Hackers','The dark side of computer security.','icon.php?name=hackers.png');
-INSERT INTO topic VALUES (11,'Hardware','Computer and Electronic hardware and equipment.','icon.php?name=hardware.png');
-INSERT INTO topic VALUES (12,'Humor','Humor. Laugh, damn it!','icon.php?name=humor.png');
-INSERT INTO topic VALUES (13,'Mail','Messages from the aether.','icon.php?name=mail.png');
-INSERT INTO topic VALUES (14,'Microsoft','Microsoft Corporation and all things Bill.','icon.php?name=microsoft.png');
-INSERT INTO topic VALUES (15,'Miscellaneous','Random stuff.','icon.php?name=misc.png');
-INSERT INTO topic VALUES (16,'Money','Money, Business, and Economics.','icon.php?name=money.png');
-INSERT INTO topic VALUES (17,'Music','Music on CD, LP, MP3, and live events.','icon.php?name=music.png');
-INSERT INTO topic VALUES (18,'News','News from the outside world.','icon.php?name=news.png');
-INSERT INTO topic VALUES (19,'Privacy','Privacy in the digital age.','icon.php?name=privacy.png');
-INSERT INTO topic VALUES (20,'Security','Computer and Network security.','icon.php?name=security.png');
-INSERT INTO topic VALUES (21,'Sites','Interesting places to visit on the web.','icon.php?name=sites.png');
-INSERT INTO topic VALUES (22,'Software','Software and programming.','icon.php?name=software.png');
-INSERT INTO topic VALUES (23,'Technology','Modern and post-modern technology.','icon.php?name=technology.png');
-INSERT INTO topic VALUES (24,'XML-Weblog','XML-Weblog site management engine','icon.php?name=xml-weblog.png');
+INSERT INTO topic VALUES (1,'No Topic','No topic.','icons/blank.png');
+INSERT INTO topic VALUES (2,'Announcements','Site Information and Announcements.','icons/announce.png');
+INSERT INTO topic VALUES (3,'Apple','Apple Computer, Mac OS X, and all things Macintosh.','icons/apple.png');
+INSERT INTO topic VALUES (4,'BSD','BSD and Berkeley-derived Unices: FreeBSD, NetBSD, OpenBSD, and Darwin.','icons/bsd.png');
+INSERT INTO topic VALUES (5,'Books','Book news and reviews.','icons/books.png');
+INSERT INTO topic VALUES (6,'Bugs','xml-weblog bug reports.','icons/bugs.png');
+INSERT INTO topic VALUES (7,'Censorship','censorship and attacks on free speech principles.','icons/censorship.png');
+INSERT INTO topic VALUES (8,'Courts','The Courts and Legal matters.','icons/courts.png');
+INSERT INTO topic VALUES (9,'Games','Computer, board, card, and other games.','icons/games.png');
+INSERT INTO topic VALUES (10,'Hackers','The dark side of computer security.','icons/hackers.png');
+INSERT INTO topic VALUES (11,'Hardware','Computer and Electronic hardware and equipment.','icons/hardware.png');
+INSERT INTO topic VALUES (12,'Humor','Humor. Laugh, damn it!','icons/humor.png');
+INSERT INTO topic VALUES (13,'Mail','Messages from the aether.','icons/mail.png');
+INSERT INTO topic VALUES (14,'Microsoft','Microsoft Corporation and all things Bill.','icons/microsoft.png');
+INSERT INTO topic VALUES (15,'Miscellaneous','Random stuff.','icons/misc.png');
+INSERT INTO topic VALUES (16,'Money','Money, Business, and Economics.','icons/money.png');
+INSERT INTO topic VALUES (17,'Music','Music on CD, LP, MP3, and live events.','icons/music.png');
+INSERT INTO topic VALUES (18,'News','News from the outside world.','icons/news.png');
+INSERT INTO topic VALUES (19,'Privacy','Privacy in the digital age.','icons/privacy.png');
+INSERT INTO topic VALUES (20,'Security','Computer and Network security.','icons/security.png');
+INSERT INTO topic VALUES (21,'Sites','Interesting places to visit on the web.','icons/sites.png');
+INSERT INTO topic VALUES (22,'Software','Software and programming.','icons/software.png');
+INSERT INTO topic VALUES (23,'Technology','Modern and post-modern technology.','icons/technology.png');
+INSERT INTO topic VALUES (24,'XML-Weblog','XML-Weblog site management engine','icons/xml-weblog.png');
 
 INSERT INTO user VALUES (1,'admin','$1$djcMXWAa$C7BumIUNqvkpTcabiU9IT0',1);
 INSERT INTO user VALUES (2,'loki','$1$dk3BJfNA$0u1ZFF3PvBy24L0Z.wZdb/',1);
