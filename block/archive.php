167e72b04cd7cd0c9da263cfb62ae90a1ca26c06
jabenninghoff <jbenninghoff@mac.com>
2002-11-25 00:46:19 -0600

    only draw block on main page (index.php)

--- block/archive.php
+++ block/archive.php
@@ -1,17 +1,20 @@
-<block>
-  <title>Archives</title>
-  <content>
 <?php
-$e = date_to_datenum($article[0]['date']);
-$a = end($article);
-$s = date_to_datenum($a['date']);
+if (basename($_SERVER['PHP_SELF']) == "index.php") {
+    echo "<block>\n";
+    echo "  <title>Archives</title>\n";
+    echo "  <content>\n";
 
-if ($start || $end) {
-    echo "<a href=\"index.php?end=$e\">Newer Articles</a><br class=\"br\"/>\n";
-} else {
-    echo "Newer Articles<br class=\"br\"/>\n";
+    $e = date_to_datenum($article[0]['date']);
+    $a = end($article);
+    $s = date_to_datenum($a['date']);
+
+    if ($start || $end) {
+        echo "<a href=\"index.php?end=$e\">Newer Articles</a><br class=\"br\"/>\n";
+    } else {
+        echo "Newer Articles<br class=\"br\"/>\n";
+    }
+    echo "<a href=\"index.php?start=$s\">Older Articles</a><br class=\"br\"/>\n";
+    echo "  </content>\n";
+    echo "</block>\n";
 }
-echo "<a href=\"index.php?start=$s\">Older Articles</a><br class=\"br\"/>\n";
 ?>
-  </content>
-</block>
