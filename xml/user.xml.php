a5791396968e1e82a68be278581d1e6e55935824
jabenninghoff <jbenninghoff@mac.com>
2003-04-16 16:32:16 -0500

    add user page stub. not implemented yet.

---
+++ xml/user.xml.php
@@ -0,0 +1,70 @@
+<?php
+// $Id: user.xml.php,v 1.1 2003/04/16 21:32:16 loki Exp $
+
+/*
+ * Copyright (c) 2002, John Benninghoff <john@benninghoff.org>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by John Benninghoff.
+ * 4. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+require_once "include/article.inc.php";
+require_once "include/db.inc.php";
+require_once "include/functions.inc.php";
+
+if (basename($_SERVER['PHP_SELF']) == "user.xml.php") {
+    // standalone
+    header('Content-Type: text/xml');
+}
+
+// build variables
+$site = fetch_site(base_url());
+$block = fetch_block();
+
+$id = valid_ID($_GET['id']);
+$topic = fetch_topic();
+$article = fetch_article_single($id);
+
+xml_declaration();
+?>
+<page lang="en" title="<?php echo $site['name']; ?>">
+
+<?php require "xml/header.xml.php"; ?>
+
+<?php require "xml/sidebar.xml.php"; ?>
+
+  <!-- main: main section of document. index page contains articles. -->
+  <main>
+<p>Not implemented.</p>
+  </main>
+
+<?php require "xml/footer.xml.php"; ?>
+
+</page>
