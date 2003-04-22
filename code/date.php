fa994d1f00876d8316e529e322004868ee3cf597
jabenninghoff <jbenninghoff@mac.com>
2003-04-22 16:50:52 -0500

    working sidebar.xml.php under new regime

--- code/date.php
+++ code/date.php
@@ -1,5 +1,5 @@
 <?php
 echo "<b>";
-if ($_SERVER['PHP_AUTH_USER']) echo xwl_valid_string($_SERVER['PHP_AUTH_USER']), " @ ";
+if ($_SERVER['PHP_AUTH_USER']) echo htmlspecialchars($_SERVER['PHP_AUTH_USER']), " @ ";
 echo date("l, F j Y", time()), "</b>";
 ?>
