50e0301f854fb2bc1112366c06beb7a8a1fe10ba
jabenninghoff <jbenninghoff@mac.com>
2004-04-30 15:57:44 -0500

    add feed:// URI for RSS browsers and fix RSS validation code to work in sites residing in subdirectories

---
+++ code/feeduri.php
@@ -0,0 +1,9 @@
+<?php
+// feed URI link builder
+// see http://www.25hoursaday.com/draft-obasanjo-feed-URI-scheme-02.html
+$uri = "feed://".$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'];
+$uri = preg_replace("/[^\/]+$/","rss.php", $uri);
+
+echo 'RSS browsers supporting the <a href="http://www.25hoursaday.com/draft-obasanjo-feed-URI-scheme-02.html">feed URI scheme</a> ',
+    'can get a full RSS feed <a href="', $uri, '">here</a>.';
+?>
--- code/validrss.php
+++ code/validrss.php
@@ -1,7 +1,7 @@
 <?php
 // archive.org feed validator logo/link
-$uri = "http://".$_SERVER['SERVER_NAME']."/rss.php";
-$uri = rawurlencode($uri);
+$uri = "http://".$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'];
+$uri = preg_replace("/[^\/]+$/","rss.php", $uri);
 
 echo '<a class="img" href="http://feeds.archive.org/validator/check.cgi?url=',
   $uri, '">', '<img src="images/valid-rss.gif" alt="Valid RSS!" height="31" width="88"/></a>';
