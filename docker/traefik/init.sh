5a673b097065c999a2dac6c04d731ae8eeff1508
jabenninghoff <jbenninghoff@mac.com>
2017-12-03 13:59:15 -0600

    Add directory & network create to traefik init script

--- docker/traefik/init.sh
+++ docker/traefik/init.sh
@@ -5,9 +5,11 @@ then
 	exit 1
 fi
 
-# set up data directory
+# set up data directory & network
+mkdir ./data
 echo "enter password for admin account:"
 PASSWD=`htpasswd -nBC 10 admin`
 cat traefik.toml | sed "s!admin:my-encrypted-htpasswd!$PASSWD!" >./data/traefik.toml
 touch ./data/acme.json
 chmod 600 ./data/acme.json
+docker network create proxy
