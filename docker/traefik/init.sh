d3bdf1de7195c2821cd4496156735b20b6b55b6b
jabenninghoff <jbenninghoff@mac.com>
2017-12-03 12:49:07 -0600

    Add traefik reverse proxy

---
+++ docker/traefik/init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if test -f ./data/acme.json -o test -f ./data/traefik.toml
+then
+	echo "$PWD/data/acme.json or $PWD/data/traefik.toml already exists! skipping"
+	exit 1
+fi
+
+# set up data directory
+echo "enter password for admin account:"
+PASSWD=`htpasswd -nBC 10 admin`
+cat traefik.toml | sed "s!admin:my-encrypted-htpasswd!$PASSWD!" >./data/traefik.toml
+touch ./data/acme.json
+chmod 600 ./data/acme.json
