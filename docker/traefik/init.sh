601de26fb1176bcbfe71b7b5e0c2d9463359d5fc
jabenninghoff <jbenninghoff@mac.com>
2017-12-07 07:07:36 -0600

    Add https by default to traefik

--- docker/traefik/init.sh
+++ docker/traefik/init.sh
@@ -11,15 +11,17 @@ then
 	mkdir ./data
 fi
 
-if [ -f ./data/acme.json -o -f ./data/traefik.toml ]
+if [ ! -f ./data/traefik.toml ]
 then
-	echo "$PWD/data/acme.json or $PWD/data/traefik.toml already exists! skipping"
-	exit 1
+	echo "enter password for admin account:"
+	PASSWD=`htpasswd -nBC 10 admin`
+	cat traefik.toml | sed "s!admin:my-encrypted-htpasswd!$PASSWD!" | sed "s/admin@example.com/$EMAIL/"  >./data/traefik.toml
 fi
 
-echo "enter password for admin account:"
-PASSWD=`htpasswd -nBC 10 admin`
-cat traefik.toml | sed "s!admin:my-encrypted-htpasswd!$PASSWD!" >./data/traefik.toml
-touch ./data/acme.json
-chmod 600 ./data/acme.json
+if [ ! -f ./data/acme.json ]
+then
+	touch ./data/acme.json
+	chmod 600 ./data/acme.json
+fi
+
 docker network create -o "com.docker.network.bridge.host_binding_ipv4"="$HOST_BIND_IP" public
