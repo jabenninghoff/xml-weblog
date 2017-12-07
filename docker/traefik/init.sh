116ad6ddc3c32019bbad7d5df237b586c0382af5
jabenninghoff <jbenninghoff@mac.com>
2017-12-07 07:16:50 -0600

    Improved init script

--- docker/traefik/init.sh
+++ docker/traefik/init.sh
@@ -2,26 +2,34 @@
 # set up data directory & network
 if [ ! -f .env ]
 then
+	echo "Installing default .env"
 	cp template.env .env
 fi
 . .env
 
 if [ ! -d ./data ]
 then
+	echo "Adding data directory..."
 	mkdir ./data
 fi
 
 if [ ! -f ./data/traefik.toml ]
 then
+	echo "Installing traefik.toml..."
 	echo "enter password for admin account:"
 	PASSWD=`htpasswd -nBC 10 admin`
-	cat traefik.toml | sed "s!admin:my-encrypted-htpasswd!$PASSWD!" | sed "s/admin@example.com/$EMAIL/"  >./data/traefik.toml
+	cat traefik.toml | sed "s!admin:my-encrypted-htpasswd!$PASSWD!" | sed "s/my-email/$EMAIL/"  >./data/traefik.toml
 fi
 
 if [ ! -f ./data/acme.json ]
 then
+	echo "Adding acme.json..."
 	touch ./data/acme.json
 	chmod 600 ./data/acme.json
 fi
 
-docker network create -o "com.docker.network.bridge.host_binding_ipv4"="$HOST_BIND_IP" public
+if [ "`docker network ls | awk '{print $2}' | grep public`" != "public" ]
+then
+	echo "Adding docker network..."
+	docker network create -o "com.docker.network.bridge.host_binding_ipv4"="$HOST_BIND_IP" public
+fi
